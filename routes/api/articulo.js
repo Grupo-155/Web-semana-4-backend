b4a77d5709df7996314f76820a1f86c161838f98
Joserdev <jcastillo@infotransgroup.com>
2020-12-18 17:43:08 -0500

    Cambios finales para las pruebas modificando el modelo articulos

---
+++ routes/api/articulo.js
@@ -0,0 +1,16 @@
+/* import routerx from 'express-promise-router';
+import articuloController from '../controllers/ArticuloController';
+import auth from '../middlewares/auth'; */
+const routerx = require('express-promise-router');
+const articuloController = require('../../controllers/ArticuloController');
+const auth = require('../../middlewares/auth');
+
+const router = routerx();
+
+router.get('/list', articuloController.list);
+router.post('/add', auth.verifyUsuario, articuloController.add);
+router.put('/update', auth.verifyUsuario, articuloController.update);
+router.put('/activate', auth.verifyUsuario, articuloController.activate);
+router.put('/deactivate', auth.verifyUsuario, articuloController.deactivate);
+
+module.exports = router;
